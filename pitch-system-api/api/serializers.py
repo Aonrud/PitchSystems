50aded60632864523ce3a3ce79f30a898cc9e7e6
Aonrud <aonrud@fastmail.com>
2024-07-12 09:38:58 +0100

    [PS-2] fix typo in interval model and serialize ratio to single field for API output

--- pitch-system-api/api/serializers.py
+++ pitch-system-api/api/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from pitches.models import *
 
 class IntervalSerializer(serializers.ModelSerializer):
+    ratio = serializers.SerializerMethodField()
+
+    def get_ratio(self, obj):
+        if obj.ratio_numerator is None:
+            return None
+        else:
+            return f'{obj.ratio_numerator}:{obj.ratio_denominator}'
+
     class Meta:
         model = Interval
-        fields = ("name", "description", "cents", "ratio_enumerator", "ratio_denominator")
+        fields = ("name", "description", "cents", "ratio")
--- pitch-system-api/pitches/models.py
+++ pitch-system-api/pitches/models.py
@@ -5,7 +5,7 @@ class Interval(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     cents = models.DecimalField(max_digits=10, decimal_places=4)
-    ratio_enumerator = models.IntegerField(null=True,blank=True)
+    ratio_numerator = models.IntegerField(null=True,blank=True)
     ratio_denominator = models.IntegerField(null=True,blank=True)
 
 
