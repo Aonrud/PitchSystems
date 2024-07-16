ed43386add80a675e99c83f86cfdb468f0db3ff6
Aonrud <aonrud@fastmail.com>
2024-07-16 14:49:34 +0100

    feat: update csv to fixtures script and add 12-ET intervals to data

--- scripts/csv2django.py
+++ scripts/csv2django.py
@@ -1,7 +1,7 @@
 #!/bin/python
-'''
+"""
 Convert from a CSV format to a Django fixture JSON file.
-'''
+"""
 
 from csv import DictReader
 import sys
@@ -9,11 +9,31 @@ import json
 from pathlib import Path
 
 
-def usage():
+def usage() -> None:
     print("Usage: csv2django CSVFILE MODEL_NAME")
 
+def type_csv_row(row: dict) -> dict:
+    '''
+    Convert string values of a dict to integer, float or None types
+    '''
+    for col in row.items():
+
+        # Null values
+        if len(col[1]) == 0 or col[1].lower() in ( "null", "none"):
+            row[col[0]] = None
+
+        # Numeric values - Try int, then float
+        try:
+            row[col[0]] = int(col[1])
+        except:
+            try:
+                row[col[0]] = float(col[1])
+            except:
+                continue
+    return row
+
 args = sys.argv
-if (len(args) < 3):
+if len(args) < 3:
     usage()
 
 csv_file = args[1]
@@ -24,10 +44,11 @@ i = 1
 
 with open(csv_file) as data:
     reader = DictReader(data)
-    for row in reader:  
-        fixtures.append({ "model": model, "pk": i, "fields": row })
-        i+=1
+    for row in reader:
+        row = type_csv_row(row)
+        fixtures.append({"model": model, "pk": i, "fields": row})
+        i += 1
 
-out_file = open(output, 'x')
+out_file = open(output, "x")
 out_file.write(json.dumps(fixtures))
 out_file.close()
