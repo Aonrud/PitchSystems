e7261717437dc663d9fbbcc395b0960c945bc55f
Aonrud <aonrud@fastmail.com>
2024-07-22 15:32:32 +0100

    build(DB_API): add allowed hosts to env for deployment

--- pitch-system-api/pitch_systems_api/settings.py
+++ pitch-system-api/pitch_systems_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", os.environ.get("API_HOST")]
 
 # Settings for REST Framework extension
 REST_FRAMEWORK = {
