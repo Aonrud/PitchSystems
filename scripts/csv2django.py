aabdf6e5e254c596061e0eb42c262c973ef90ebb
Aonrud <aonrud@fastmail.com>
2024-09-07 11:14:24 +0100

    chore(scripts): add in final versions of fixtures scripts

--- scripts/csv2django.py
+++ scripts/csv2django.py
@@ -7,10 +7,47 @@ from csv import DictReader
 import sys
 import json
 from pathlib import Path
+import math
 
 
 def usage() -> None:
     print("Usage: csv2django CSVFILE MODEL_NAME")
+
+# Allowed deviation between cents value given and an interval to be considered equal
+CENTS_TOLERANCE = 0.0005
+# Can start with higher id when merging with an existing list
+PK_start = 86
+
+interval_file = open(
+    (Path(__file__).parent / "../pitch-systems-api/fixtures/interval.json").resolve(),
+    "r",
+)
+intervals = json.load(interval_file)
+
+def find_intervals(value: str) -> list:
+    """
+    Find interval IDs by cents value.
+    """
+    cents_list = value.split(",")
+    ids = []
+
+    for cents in cents_list:
+        int_id = next(
+                    (
+                        interval["pk"]
+                        for interval in intervals
+                        # Match on isclose() to allow for rounding/float imprecision.
+                        if math.isclose(
+                            interval["fields"]["cents"],
+                            float(cents),
+                            abs_tol=CENTS_TOLERANCE,
+                        )
+                    ),
+                    None
+                )
+        ids.append(int_id)
+    return ids
+
 
 def type_csv_row(row: dict) -> dict:
     '''
@@ -18,18 +55,26 @@ def type_csv_row(row: dict) -> dict:
     '''
     for col in row.items():
 
-        # Null values
-        if len(col[1]) == 0 or col[1].lower() in ( "null", "none"):
-            row[col[0]] = None
+        # Special case of intervals - search the interval fixtures for matches and get the IDs
+        if col[0] in ["intervals", "interval"]:
+            print(f"{col[1]} matches {find_intervals(col[1])}")
+            match = find_intervals(col[1])
+            if col[0] == "interval":
+                match = match[0]
+            row[col[0]] = match
+        else:
+            # Null values
+            if len(col[1]) == 0 or col[1].lower() in ( "null", "none"):
+                row[col[0]] = None
 
-        # Numeric values - Try int, then float
-        try:
-            row[col[0]] = int(col[1])
-        except:
+            # Numeric values - Try int, then float
             try:
-                row[col[0]] = float(col[1])
+                row[col[0]] = int(col[1])
             except:
-                continue
+                try:
+                    row[col[0]] = float(col[1])
+                except:
+                    continue
     return row
 
 args = sys.argv
@@ -40,7 +85,7 @@ csv_file = args[1]
 output = Path(csv_file).stem + ".json"
 model = args[2]
 fixtures = []
-i = 1
+i = PK_start
 
 with open(csv_file) as data:
     reader = DictReader(data)
--- scripts/scl2django.py
+++ scripts/scl2django.py
@@ -21,10 +21,14 @@ class SclParser:
     # Allowed deviation between cents value given and an interval to be considered equal
     CENTS_TOLERANCE = 0.0005
 
-    intervals: dict
+    intervals: list
+    sys_map: dict
+    missing: list
 
-    def __init__(self, intervals: list):
+    def __init__(self, intervals: list, sys_map: dict):
         self.intervals = intervals
+        self.sys_map = sys_map
+        self.missing = []
 
     def parse(self, filename: str) -> object:
         """Parse an scl file and return an object
@@ -45,64 +49,103 @@ class SclParser:
             "intervals": [],
         }
 
-        # TODO: For robustness, check that the count matches the number of intervals received.
-        for line in scl:
-            # Ignore comment lines
-            if line[0] == "!":
-                continue
+        try:
+            # TODO: For robustness, check that the count matches the number of intervals received.
+            for line in scl:
+                # Ignore comment lines
+                if line[0] == "!":
+                    continue
 
-            if scale["name"] == "":
-                scale["name"] = line.strip()
-            elif scale["count"] == 0:
-                scale["count"] = int(line)
-            else:
-                scale["intervals"].append(self.get_interval_id(line))
+                #Truncate end of line comments
+                if "!" in line:
+                    line = line.partition("!")[0]
 
-        # Count isn't part of Scale model
-        del scale["count"]
+                # .scl files don't include a description. However, the Scala set often contains descriptive information in the title. Take post-comma content as description.
+                if scale["name"] == "":
+                    scale["name"] = line.strip()
+                elif scale["count"] == 0:
+                    scale["count"] = int(line)
+                else:
+                    scale["intervals"].append(self.get_interval_id(line))
 
-        # Scl doesn't provide description beyond a title
-        scale["description"] = scale["name"]
+            # Count isn't part of Scale model
+            del scale["count"]
 
-        return scale
+            # Scl doesn't provide description beyond a title
+            scale["description"] = ""
+
+            # If file is mapped to a system, add the system
+            key = filename.name
+            if key in sys_map:
+                scale["system"] = int(sys_map[key])
+
+            return scale
+        except UnicodeDecodeError:
+            print(f"Non-unicode file {filename}")
+        except ValueError:
+            print(f"Bad value: {line}")
+
 
     def get_interval_id(self, text: str) -> int:
         """
         Get the interval ID matching an scl interval line
         """
 
+        # Drop newlines
+        text = text.rstrip()
+
+        # Drop "cents" -- seems to be appended in some files
+        if "cents" in text:
+            text = text.partition("cents")[0].strip()
+
         # An integer without decimal point is assumed to be X/1 ratio
         if text.find(".") == -1 and text.find("/") == -1:
             text += "/1"
 
+        int_id = -1
+        cents_val = text
+
         # Handle ratios
         if text.find("/") != -1:
             parts = text.split("/")
-            return next(
+            int_id = next(
                 (
                     interval["pk"]
                     for interval in intervals
                     if interval["fields"]["ratio_numerator"] == int(parts[0])
                     and interval["fields"]["ratio_denominator"] == int(parts[1])
                 ),
-                None,
+                -1,
+            )
+            try:
+                cents_val = float(1200 * math.log2(int(parts[1]) / int(parts[0])))
+            except ValueError:
+                print(f"Invalid interval: {text}")
+
+        #No ratio match found -- try the cents value
+        if int_id < 0:
+            # Otherwise match cents value
+            int_id = next(
+                (
+                    interval["pk"]
+                    for interval in intervals
+                    # Match on isclose() to allow for rounding/float imprecision.
+                    # Difference is entirely inaudible, so essentially meaningless.
+                    if math.isclose(
+                        interval["fields"]["cents"],
+                        float(cents_val),
+                        abs_tol=self.CENTS_TOLERANCE,
+                    )
+                ),
+                -1,
             )
 
-        # Otherwise match cents value
-        return next(
-            (
-                interval["pk"]
-                for interval in intervals
-                # Match on isclose() to allow for rounding/float imprecision.
-                # Difference is entirely inaudible, so essentially meaningless.
-                if math.isclose(
-                    interval["fields"]["cents"],
-                    float(text),
-                    abs_tol=self.CENTS_TOLERANCE,
-                )
-            ),
-            None,
-        )
+        #Finally, add it to the missing intervals list
+        if int_id < 0:
+            self.missing.append(text)
+
+        return (None, int_id) [int_id > 0]
+
 
 
 args = sys.argv
@@ -116,27 +159,58 @@ if not scl_dir.is_dir():
 files = [f for f in scl_dir.iterdir() if Path.is_file(f)]
 
 interval_file = open(
-    (Path(__file__).parent / "../pitch-system-api/fixtures/interval.json").resolve(),
+    (Path(__file__).parent / "../pitch-systems-api/fixtures/interval.json").resolve(),
     "r",
 )
 intervals = json.load(interval_file)
 
-parser = SclParser(intervals)
+# Manual mapping of systems to scl files.
+sys_map: dict = {}
+
+with open("system_map.csv") as sys_file:
+    for line in sys_file:
+        try:
+            parts = line.split(",")
+            sys_map[parts[0]] = parts[1].rstrip()
+        except IndexError:
+            print(f"Bad sys_map line: {line}")
+
+print(sys_map)
+
+
+parser = SclParser(intervals, sys_map)
 
 fixtures = []
 i = 1
 
-for file in files:
-    fixtures.append(
-        {
-            "pk": i,
-            "model": "pitches.scale",
-            "fields": parser.parse(file)
-        }
-    )
-    i += 1
+incomplete_file = open("incomplete", "w")
 
-pprint(fixtures)
-out_file = open("scales.json", "x")
+
+for f in files:
+
+    # Drop scales with unmatched intervals
+    results = parser.parse(f)
+    if results and None in results["intervals"]:
+        incomplete_file.write(f"{f}\n")
+    else:
+         # TMP: Drop if system unmatched
+        if results and "system" in results:
+            fixtures.append(
+                {
+                    "pk": i,
+                    "model": "pitches.scale",
+                    "fields": results
+                }
+            )
+            i += 1
+        else:
+            print(f"No system: {results}")
+
+
+pprint(parser.missing)
+incomplete_file.close()
+
+# Write the JSON fixtures file
+out_file = open("scale.json", "x")
 out_file.write(json.dumps(fixtures))
 out_file.close()
